cf7589e803495dd6041a3f9cba2cd0bee0dcec29
YifanMa <myf990101@gmail.com>
2025-11-04 10:39:41 +0800

    测试间场景下_M3和M4

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/lift_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/lift_env_cfg.py
@@ -493,7 +493,7 @@ class LiftEnvCfg(ManagerBasedRLEnvCfg):
         self.episode_length_s = 0.6
         # simulation settings
         self.sim.dt = 0.01 # 100Hz
-        self.sim.render_interval =10
+        self.sim.render_interval =1
 
         self.sim.physx.bounce_threshold_velocity = 0.2
         self.sim.physx.bounce_threshold_velocity = 0.01
